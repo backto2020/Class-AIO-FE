4f9d7f85ce8fd178ce84c250fa9f9e62281bf2b9
backto2020 <backto2020@foxmail.com>
2022-05-29 00:40:23 +0800

    feat: commit APIs

---
+++ src/renderer/api/commit.js
@@ -0,0 +1,45 @@
+import request from '@/utils/request';
+
+export function deleteCommit(id) {
+  return request({
+    url: `/commit/${id}`,
+    method: 'delete'
+  });
+}
+
+export function editCommit(commit) {
+  return request({
+    url: '/commit',
+    method: 'post',
+    data: commit
+  });
+}
+
+export function getCommitList() {
+  return request({
+    url: '/commit',
+    method: 'get'
+  });
+}
+
+export function newCommit(commit) {
+  return request({
+    url: '/commit',
+    method: 'put',
+    data: commit
+  });
+}
+
+export function loginCommit(id) {
+  return request({
+    url: `/commit/${id}`,
+    method: 'put'
+  });
+}
+
+export function getCommitStatus(id) {
+  return request({
+    url: `/commit/${id}`,
+    method: 'get'
+  });
+}
