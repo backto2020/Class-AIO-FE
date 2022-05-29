3216fe8cee21a4dbe07fab916698de8c4396e3fd
backto2020 <backto2020@foxmail.com>
2022-05-29 16:04:30 +0800

    feat: sort notiList when init; add tags

--- src/renderer/views/notifications/index.vue
+++ src/renderer/views/notifications/index.vue
@@ -2,6 +2,8 @@
   <div>
     <el-card v-for="info in notification_list" :key="info.id" class="noti-card">
       <div slot="header" class="clearfix">
+        <el-tag v-if="info.type === '紧急'" type="danger">{{ info.type }}</el-tag>
+        <el-tag v-else>{{ info.type }}</el-tag>
         <span>{{ info.title }}</span>
       </div>
       <div class="noti-time">{{ info.timeString }}</div>
@@ -21,9 +23,13 @@ export default {
   },
   created() {
     getNotiList().then(res => {
-      this.notification_list = res.data.map(x => {
-        return { ...x, timeString: t2s(new Date(x.time)) };
-      });
+      this.notification_list = res.data
+        .map(x => {
+          return { ...x, timeString: t2s(new Date(x.time)) };
+        })
+        .sort((a, b) => {
+          return new Date(b.time).valueOf() - new Date(a.time).valueOf();
+        });
     });
   }
 };
